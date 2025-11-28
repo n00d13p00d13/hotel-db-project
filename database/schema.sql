7f3dfd6540d49417a2472ec0e03500cb6067653b
n00d13p00d13 <abdobroo9898@gmail.com>
2025-11-28 15:40:23 +0200

    add not null and remove shift staff table

--- database/schema.sql
+++ database/schema.sql
@@ -11,7 +11,7 @@ SELECT * FROM Guest;
 
 CREATE TABLE GuestPhones IF NOT EXISTS (
     PhoneID INT AUTO_INCREMENT PRIMARY KEY,
-    Phone VARCHAR(20),
+    Phone VARCHAR(20) NOT NULL,
     G_ID INT,
     FOREIGN KEY(G_ID) REFERENCES Guest(G_ID)
 );
@@ -19,31 +19,31 @@ SELECT * FROM GuestPhones;
 
 CREATE TABLE RoomType IF NOT EXISTS (
     RoomType_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Type_Name VARCHAR(20),
-    PricePerNight DECIMAL(10,2)
+    Type_Name VARCHAR(20) NOT NULL,
+    PricePerNight DECIMAL(10,2) NOT NULL
 );
 SELECT * FROM RoomType;
 
 CREATE TABLE Room (
     R_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Number VARCHAR(10) UNIQUE,
-    Floor INT,
+    Number VARCHAR(10) NOT NULL UNIQUE,
+    Floor INT NOT NULL,
     -- Status VARCHAR(20),  -- Available, Booked, Under Maintenance
-    Status ENUM('Available', 'Booked', 'Under Maintenance'),
-    RoomType_ID INT,
+    Status ENUM('Available', 'Booked', 'Under Maintenance') NOT NULL,
+    RoomType_ID INT NOT NULL,
     FOREIGN KEY (RoomType_ID) REFERENCES RoomType(RoomType_ID)
 );
 SELECT * FROM Room;
 
 CREATE TABLE Booking IF NOT EXISTS (
     B_ID INT AUTO_INCREMENT PRIMARY KEY,
-    G_ID INT,
-    R_ID INT,
-    Check_In_Date DATE,
-    Check_Out_Date DATE,
-    Booking_Date DATE,
+    G_ID INT NOT NULL,
+    R_ID INT NOT NULL,
+    Check_In_Date DATE NOT NULL,
+    Check_Out_Date DATE NOT NULL,
+    Booking_Date DATE NOT NULL,
     Meal_Plan VARCHAR(50),
-    Status ENUM('Confirmed', 'Cancelled', 'Pending'),
+    Status ENUM('Confirmed', 'Cancelled', 'Pending') NOT NULL,
     FOREIGN KEY (G_ID) REFERENCES Guest(G_ID),
     FOREIGN KEY (R_ID) REFERENCES Room(R_ID)
 );
@@ -51,23 +51,23 @@ SELECT * FROM booking;
 
 CREATE TABLE Payment IF NOT EXISTS (
     Pay_ID INT AUTO_INCREMENT PRIMARY KEY,
-    B_ID INT,
-    Amount DECIMAL(10,2),
-    Payment_Date DATE,
-    Payment_Method ENUM('Card', 'Cash', 'Online'),
+    B_ID INT NOT NULL,
+    Amount DECIMAL(10,2) NOT NULL,
+    Payment_Date DATE NOT NULL,
+    Payment_Method ENUM('Card', 'Cash', 'Online') NOT NULL,
     FOREIGN KEY (B_ID) REFERENCES Booking(B_ID)
 );
 SELECT * FROM payment;
 
 CREATE TABLE Staff IF NOT EXISTS (
     Staff_ID INT AUTO_INCREMENT PRIMARY KEY,
-    F_name VARCHAR(20),
-    L_name VARCHAR(20),
-    Role VARCHAR(40),
-    Phone VARCHAR(20),
-    Email VARCHAR(50) UNIQUE,
-    Salary DECIMAL(10,2),
-    HireDate DATE
+    F_name VARCHAR(20) NOT NULL,
+    L_name VARCHAR(20) NOT NULL,
+    Role VARCHAR(40) NOT NULL,
+    Phone VARCHAR(20) NOT NULL,
+    Email VARCHAR(50) NOT NULL UNIQUE,
+    Salary DECIMAL(10,2) NOT NULL,
+    HireDate DATE NOT NULL
 );
 SELECT * FROM staff;
 
@@ -76,18 +76,10 @@ SELECT * FROM staff;
 --------------------------------
 CREATE TABLE Shift IF NOT EXISTS (
     Shift_ID INT AUTO_INCREMENT PRIMARY KEY,
-    ShiftName VARCHAR(50),
-    StartTime TIME,
-    EndTime TIME
+    Staff_ID INT NOT NULL,
+    ShiftName VARCHAR(50) NOT NULL,
+    StartTime TIME NOT NULL,
+    EndTime TIME NOT NULL,
+    FOREIGN KEY(Shift_ID) REFERENCES Staff(Shift_ID)
 );
 SELECT * FROM shift;
-
-CREATE TABLE Shift_Staff IF NOT EXISTS (
-    ShiftStaff_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Staff_ID INT,
-    Shift_ID INT,
-    FOREIGN KEY (Staff_ID) REFERENCES Staff(Staff_ID),
-    FOREIGN KEY (Shift_ID) REFERENCES Shift(Shift_ID)
-);
-SELECT * FROM shift_staff;
-
